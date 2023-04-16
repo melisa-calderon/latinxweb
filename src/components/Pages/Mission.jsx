46adf96d6c7f4e9d57a0cc2252986af86db1d852
melisa-calderon <mecalderon@vassar.edu>
2023-04-16 14:15:56 -0400

    add

--- src/components/Pages/Mission.jsx
+++ src/components/Pages/Mission.jsx
@@ -9,10 +9,12 @@ const Mission = () => {
             
                 <h1>Mission</h1>
                 <p>
-                    The overall goal of this website is to provide pregnant teens and/or their guardians with the necessary resources for teen mothers to continue their education. Since education is a social determinant of health and has strong correlation with economic prosperity, helping pregnant teens continue and finish their education is essential in their success.
+                    The overall goal of this website is to provide pregnant teens
+                     and/or their guardians with the necessary resources for teen 
+                     mothers to continue their education. Teenage parents face numerous responsibilities that can hinder their ability to continue their education. Studies show that “Overall, 53 percent of young women who gave birth as teens received a high school diploma, compared with 90 percent of those who did not.” (Lantos and Manlove 2020). These statistics are significant because education plays a vital role in improving the quality of life for both the parents and their child. Education is a social determinant of health and has a strong correlation with economic prosperity. Therefore, ensuring that pregnant teenagers have the opportunity to continue and finish their education is essential in their success.
                 </p>
                 <p>
-                    Education is not a federal right, so education programs vary by state and local level. This website will focus on Texas, but the website identifies rights for all pregnant individuals at the federal level.
+                    Education is not a federal right, so education programs vary by state and local level. This website will focus on Texas and New York, but the website identifies rights for all pregnant individuals at the federal level.
                 </p>
                 <p>
                     Specifically, this website is aimed at providing Latinx communities with the information necessary to make the process of continuing education smoother. This website can be viewed in Spanish and English, with English as the default setting. However, all ethnic groups are welcome to use the website.
@@ -20,7 +22,7 @@ const Mission = () => {
                 <h2>Education is an investment</h2>
                 <img src="../../public/img/earning-by-edu.png" width="100%" alt="graph"></img>
                 <p>
-                    Having a high school degree improves the weekly take home salary and having a higher level degree further improves the weekly earnings as seen above. While working towards the degree might seem daunting, the outcome are much more favorable in the future. For teenage parent, this can be challenging since they have to take care of a child, which is a job by itself. Moreover, having children comes with extra expenses like clothes, formula, child care and food. As a result, many parents drop out and decide to join the workforce to provide for their new family. Additionally, it can be extremely challenging to juggle taking care of a child, having a job, and going to school. However, there are programs that assist teens continue their education through affordable child care and extra resources.
+                    Having a high school degree improves the weekly take home salary, and having a higher level degree further improve weekly earnings as seen above. While working towards a degree might seem daunting, the long-term benefits are substantial. For teenage parent, this can be challenging since they have to take care of a child, which is a job by itself. Moreover, having children comes with extra expenses like clothes, formula, child care and food. As a result, many parents drop out and decide to join the workforce to provide for their new family. Additionally, it can be extremely challenging to juggle taking care of a child, having a job, and going to school. However, there are programs that assist teens continue their education through affordable child care and extra resources.
                 </p>
             </div>
         </div>
---
+++ src/components/Pages/NewYork/NY.js
@@ -0,0 +1,33 @@
+export const pages = [
+    {
+      id: 0,
+      name: "LYFE",
+      img_scr: "locker.jpg",
+      description:
+        "Here you can find the mission of the website. This section includes information on why completing one’s education is important as well as the positive impacts it can have on a teen’s economic and personal future.  Click here to explore our mission.",      
+    },
+    {
+      id: 1,
+      name: "The Door",
+      img_scr: "", 
+      description:
+        "Here you can find the mission of the website. This section includes information on why completing one’s education is important as well as the positive impacts it can have on a teen’s economic and personal future.  Click here to explore our mission.",      
+    },
+    {
+      id: 2,
+      name: "Good Sheperd Services",
+      img_scr: "", 
+      description:
+        "Here you can find resources for pregnant teens who are attending federally funded schools. In addition, the page provides further information for teens in the state of Texas. Click here to explore your educational rights as a pregnant teen.",
+      
+    },
+    {
+      id: 3,
+      name: "New York",
+      img_scr: "", 
+      description:
+        "Here you can find resources for  teen mothers who are attending federally funded schools. The page provides further information on programs for teens in the state of Texas. Click here to explore your educational rights after a pregnancy.",
+      
+    },
+    
+  ];
