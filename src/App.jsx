d4005ccd34686316e5efc448829f56def85e7a8d
melisa-calderon <mecalderon@vassar.edu>
2023-04-16 22:27:13 -0400

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ const App = () => {
   return (
     <>
       <Routes>
-        <Route path="" element={<Home />} />
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/mission" element={<Mission />} />
