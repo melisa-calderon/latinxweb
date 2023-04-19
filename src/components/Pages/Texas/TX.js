3de5ce8c20785d3fca4dd9cf30fb4099ccdf50e8
melisa-calderon <mecalderon@vassar.edu>
2023-04-19 00:46:58 -0400

    fixed one minor bug

--- src/components/Pages/Texas/TX.js
+++ src/components/Pages/Texas/TX.js
@@ -6,23 +6,9 @@ export const txres = [
       description:
         "AVANCE is a nonprofit organization that provides parenting education, early childhood development programs, and adult education services to low-income families in Texas. They have programs specifically designed to support teenage Latina mothers in completing their education.",      
     },
+    
     {
       id: 1,
-      name: "The Door",
-      img_scr: "", 
-      description:
-        "The Door is a non-profit organization that provides a range of services to at-risk youth, including pregnant and parenting teens. The organization offers academic support, counseling, and parenting classes.",
-    },
-    {
-      id: 2,
-      name: "Good Sheperd Services",
-      img_scr: "", 
-      description:
-        "Good Shepherd Services is a non-profit organization that provides education, counseling, and support to at-risk youth, including pregnant and parenting teens. The organization offers a range of services, including academic instruction, parenting classes, and childcare.",
-      
-    },
-    {
-      id: 3,
       name: "New York",
       img_scr: "", 
       description:
